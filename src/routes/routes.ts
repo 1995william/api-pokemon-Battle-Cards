27515a37225306fdd35e81d0e9ae0c2d24b6e610
Will <rwps1234@gmail.com>
2022-08-06 17:29:42 -0300

    feat: adding new route GET pokemons

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,6 @@ import { PokemonController } from "../controllers/pokemonController";
 const routes = express.Router();
 
 routes.get("/", PokemonController.home);
-
+routes.get("/pokemons", PokemonController.findAll);
 
 export { routes };
