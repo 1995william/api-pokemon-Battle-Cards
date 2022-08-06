0745a0c65be5c21ae93d83b21e888a7dfb31299c
Will <rwps1234@gmail.com>
2022-08-06 17:26:30 -0300

    feat: creating findAll method

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -5,5 +5,8 @@ export class PokemonController {
     static home = (req: Request, res: Response) => {
         res.json("Welcome to api pokemon battle cards");
       }
-      
+    
+    static findAll = (req: Request, res: Response) => {
+        res.status(200).json("GET all pokemóns");
+      }
 }
