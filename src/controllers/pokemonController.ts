0511f813a50fb6d53871912932b40934e6ac6a31
Will <rwps1234@gmail.com>
2022-08-06 18:32:57 -0300

    feat: creating update method

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from "express";
 
 export class PokemonController {
+  static home = (req: Request, res: Response) => {
+    res.json("Welcome to api pokemon battle cards");
+  };
 
-    static home = (req: Request, res: Response) => {
-        res.json("Welcome to api pokemon battle cards");
-      }
-    
-    static findAll = (req: Request, res: Response) => {
-        res.status(200).json("GET all pokemóns");
-      }
-    static register = (req: Request, res: Response) => {
-        const newPokemon = req.body;
+  static findAll = (req: Request, res: Response) => {
+    res.status(200).json("GET all pokemóns");
+  };
 
-        res.status(201).json({"pokemon registered successfully":newPokemon});
-      }
+  static register = (req: Request, res: Response) => {
+    const newPokemon = req.body;
+
+    res.status(201).json({ "pokemon registered successfully": newPokemon });
+  };
+
+  static update = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const updatePokemon = req.body;
+
+    res.status(200).json({ "pokemon updated successfully": updatePokemon });
+  };
 }
