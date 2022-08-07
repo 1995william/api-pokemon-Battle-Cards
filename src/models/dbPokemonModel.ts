31dd1054d4700fdcc396ad7487650c84efc3a604
Will <rwps1234@gmail.com>
2022-08-06 23:33:33 -0300

    feat: creating pokemonSchema

---
+++ src/models/dbPokemonModel.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const pokemonSchema = new Schema({
+  name: { type: String, required: true },
+  attributes: {
+    type: {
+      hp: Number,
+      attack: Number,
+      defense: Number,
+      "special-attack": Number,
+      "special-defense": Number,
+      speed: Number,
+    },
+    required: true,
+  },
+});
+
+const pokemonModel = mongoose.model("pokemon", pokemonSchema);
+
+export { pokemonModel };
