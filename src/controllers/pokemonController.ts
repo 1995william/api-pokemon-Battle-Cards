2797c4ba89480c71d0f86e447e6231d93bd5c6e2
Will <rwps1234@gmail.com>
2022-08-08 19:15:12 -0300

    feat: add new route put by name

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -40,7 +40,6 @@ export class PokemonController {
     }
   };
 
-
   static register = async (req: Request, res: Response) => {
     const newPokemon = req.body;
     try {
@@ -52,11 +51,31 @@ export class PokemonController {
     }
   };
 
-  static update = (req: Request, res: Response) => {
+  static updateById = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const updatePokemon = req.body;
+    const pokemon = req.body;
+    try {
+     const pokemonUpdated = await pokemonModel.findOneAndUpdate({_id:id}, pokemon);
+     if(pokemonUpdated === null) return res.status(404).json({ "Pokemon not found _id": id });
+      res.status(200).json({ "pokemon updated successfully": pokemonUpdated });
+      
+    } catch (error:any) {
+      res.status(404).json({ "error in request": error.message });
+    }
+  };
 
-    res.status(200).json({ "pokemon updated successfully": updatePokemon });
+  static updateByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
+    const pokemon = req.body;
+    try {
+     const pokemonUpdated = await pokemonModel.findOneAndUpdate({name: name}, pokemon);
+
+      if(pokemonUpdated === null) return res.status(404).json({ "Pokemon not found name": name }) ;
+      res.status(200).json({ "pokemon updated successfully": pokemonUpdated });
+      
+    } catch (error:any) {
+      res.status(404).json({ "error in request": error.message });
+    }
   };
 
   static remove = async (req: Request, res: Response) => {
