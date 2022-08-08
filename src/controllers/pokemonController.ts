2cc879b6ea744ceea954ff5fe1f81b7a9923834b
Will <rwps1234@gmail.com>
2022-08-08 18:14:53 -0300

    feat: CRUD working

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -3,27 +3,52 @@ import { pokemonModel } from "../models/dbPokemonModel";
 
 export class PokemonController {
   static home = (req: Request, res: Response) => {
-    res.json("Welcome to api pokemon battle cards");
+    res.status(200).json("Welcome to api pokemon battle cards");
   };
 
   static findAll = async (req: Request, res: Response) => {
     try {
      const pokemons = await pokemonModel.find();
-     res.status(200).json(pokemons);
+     const pokeOrderAsc = pokemons.sort((a,b)=> a._id - b._id);
+     
+     res.status(200).json(pokeOrderAsc);
       
     } catch (error:any) {
       res.status(404).json({"error in response": error.message})
     }
   };
 
+  static findOneByid = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+     const pokemons = await pokemonModel.findOne({_id: id});
+     res.status(200).json(pokemons);
+      
+    } catch (error:any) {
+      res.status(404).json({"error in response": error.message});
+    }
+  };
+
+  static findOneByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
+    try {
+     const pokemons = await pokemonModel.findOne({name: name});
+     res.status(200).json(pokemons);
+      
+    } catch (error:any) {
+      res.status(404).json({"error in response": error.message});
+    }
+  };
+
+
   static register = async (req: Request, res: Response) => {
     const newPokemon = req.body;
     try {
-        await pokemonModel.create(newPokemon)
+        await pokemonModel.create(newPokemon);
         res.status(201).json({ "pokemon registered successfully": newPokemon });
         
     } catch (error:any) {
-        res.status(404).json({ "error in request": error.message })
+        res.status(404).json({ "error in request": error.message });
     }
   };
 
@@ -35,14 +60,14 @@ export class PokemonController {
   };
 
   static remove = async (req: Request, res: Response) => {
-    const { _id } = req.params;
-    console.log(_id)
+    const { id } = req.params;
+    
     try {
-      const pokemonRemoved = await pokemonModel.findByIdAndRemove(_id)
+      const pokemonRemoved = await pokemonModel.findByIdAndRemove(id);
       res.status(200).json({ "pokemon successfully removed": pokemonRemoved });
       
     } catch (error:any) {
-      res.status(404).json({ "error in request": error.message })
+      res.status(404).json({ "error in request": error.message });
     }
   };
 }
