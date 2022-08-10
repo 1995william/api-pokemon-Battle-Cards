3775669e2b79fe389e095ae4a138f9134f6e3055
Will <rwps1234@gmail.com>
2022-08-09 21:29:15 -0300

    chore: method battle finished

--- src/controllers/battleController.ts
+++ src/controllers/battleController.ts
@@ -5,35 +5,80 @@ export class BattleController {
   static arena = async (req: Request, res: Response) => {
     const { playerOneCard, playerTwoCard } = req.body;
     
-   const battleResult = await this.battle(playerOneCard,playerTwoCard);
-    console.log(battleResult)
-    res.status(200).json(battleResult);
-};
+    if(!playerOneCard) return res.status(404).json({"Player is required": "playerOneCard"});
+    if(!playerTwoCard) return res.status(404).json({"Player is required": "playerTwoCard"});
 
+    try {
+      const battleResult = await this.battle(playerOneCard, playerTwoCard);
+      res.status(201).json(battleResult);
+    } catch (error) {
 
+      const pokemons = await pokemonModel.find();
+      pokemons.find(el => el._id === playerOneCard) === undefined ?
+        res.status(404).json({
+          "Pokemon not found _id": playerOneCard,
+          "error": error.message
+        }) :
+        res.status(404).json({
+          "Pokemon not found _id": playerTwoCard,
+          "error": error.message
+        });
+    }
 
-static battle = async (playerOne: number, playerTwo: number) => {
+  };
 
+  static battle = async (playerOne: number, playerTwo: number) => {
     const playerOneCard = await pokemonModel.findOne({ _id: playerOne });
     const playerTwoCard = await pokemonModel.findOne({ _id: playerTwo });
 
     let playerOneAttributeWin = 0;
     let playerTwoAttributeWin = 0;
 
-     // compara os atributos das cartas
-    playerOneCard.attributes.hp > playerTwoCard.attributes.hp ? playerOneAttributeWin++ : playerTwoAttributeWin++     
-    playerOneCard.attributes.attack > playerTwoCard.attributes.attack ? playerOneAttributeWin++ : playerTwoAttributeWin++     
-    playerOneCard.attributes.defense > playerTwoCard.attributes.defense ? playerOneAttributeWin++ : playerTwoAttributeWin++     
-    playerOneCard.attributes["special-attack"] > playerTwoCard.attributes["special-attack"] ? playerOneAttributeWin++ : playerTwoAttributeWin++     
-    playerOneCard.attributes["special-defense"] > playerTwoCard.attributes["special-defense"] ? playerOneAttributeWin++ : playerTwoAttributeWin++     
-    playerOneCard.attributes.speed > playerTwoCard.attributes.speed ? playerOneAttributeWin++ : playerTwoAttributeWin++
-    
-    // retona o jogador vencedor e os atributos das cartas
-    return playerOneAttributeWin > playerTwoAttributeWin ? 
-    new ResultModel(1, 2, playerOneCard.attributes) :
-    new ResultModel(2, 1, playerTwoCard.attributes)
+    // compara os atributos das cartas
 
-  }
-
-
+    if (playerOneCard.attributes.hp !== playerTwoCard.attributes.hp) {
+      playerOneCard.attributes.hp > playerTwoCard.attributes.hp
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    if (playerOneCard.attributes.attack !== playerTwoCard.attributes.attack) {
+      playerOneCard.attributes.attack > playerTwoCard.attributes.attack
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    if (playerOneCard.attributes.defense !== playerTwoCard.attributes.defense) {
+      playerOneCard.attributes.defense > playerTwoCard.attributes.defense
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    if (
+      playerOneCard.attributes["special-attack"] !==
+      playerTwoCard.attributes["special-attack"]
+    ) {
+      playerOneCard.attributes["special-attack"] >
+        playerTwoCard.attributes["special-attack"]
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    if (
+      playerOneCard.attributes["special-defense"] !==
+      playerTwoCard.attributes["special-defense"]
+    ) {
+      playerOneCard.attributes["special-defense"] >
+        playerTwoCard.attributes["special-defense"]
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    if (playerOneCard.attributes.speed !== playerTwoCard.attributes.speed) {
+      playerOneCard.attributes.speed > playerTwoCard.attributes.speed
+        ? playerOneAttributeWin++
+        : playerTwoAttributeWin++;
+    }
+    // retona empate ou o jogador e a carta vencedora com os atributos
+    console.log(playerOneAttributeWin, playerTwoAttributeWin);
+    if (playerOneAttributeWin === playerTwoAttributeWin) return "Draw";
+    return playerOneAttributeWin > playerTwoAttributeWin
+      ? new ResultModel(1, 2, playerOneCard.name, playerOneCard.attributes)
+      : new ResultModel(2, 1, playerTwoCard.name, playerTwoCard.attributes);
+  };
 }
