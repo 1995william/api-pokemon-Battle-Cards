7419952235d7fb4139b49107cbbc357b0eed41c9
Will <rwps1234@gmail.com>
2022-08-10 21:33:32 -0300

    feat: adding new routes GET and PUT scores

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import { BattleController } from "../controllers/battleController";
 import { PokemonController } from "../controllers/pokemonController";
+import { ScoreController } from "../controllers/scoreController";
 
 const routes = express.Router();
 
 routes.get("/", PokemonController.home);
 routes.get("/pokemons", PokemonController.findAll);
+routes.get("/scores", ScoreController.scoreTotal);
 routes.get("/pokemon/id/:id", PokemonController.findOneByid);
 routes.get("/pokemon/name/:name", PokemonController.findOneByName);
 routes.post("/pokemon", PokemonController.register);
 routes.post("/battle", BattleController.arena);
 routes.put("/pokemon/id/:id", PokemonController.updateById);
+routes.put("/scores", ScoreController.update);
 routes.put("/pokemon/name/:name", PokemonController.updateByName);
 routes.delete("/pokemon/id/:id", PokemonController.remove);
 
