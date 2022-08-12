2729018d47da013d3acbb0379b1ac39e476d912c
Will <rwps1234@gmail.com>
2022-08-12 13:45:07 -0300

    feat: adding pagination and limit of item in route GET

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -7,11 +7,13 @@ export class PokemonController {
   };
 
   static findAll = async (req: Request, res: Response) => {
+    const page = Number(req.query.page) || 0;
+    const limit = Number(req.query.limit) || 10;
+    
     try {
-     const pokemons = await pokemonModel.find();
-     const pokeOrderAsc = pokemons.sort((a,b)=> a._id - b._id);
+     const pokemons = await pokemonModel.find().skip(page * limit).limit(limit).sort('_id');
      
-     res.status(200).json(pokeOrderAsc);
+     res.status(200).json(pokemons);
       
     } catch (error) {
       res.status(404).json({"error in response": error.message})
