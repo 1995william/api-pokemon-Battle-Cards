396a12f3717094c1108c1b48b4aed2080885eec7
Will <rwps1234@gmail.com>
2022-08-11 22:04:33 -0300

    feat: adding error 404 if pokemon not exist

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -13,7 +13,7 @@ export class PokemonController {
      
      res.status(200).json(pokeOrderAsc);
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({"error in response": error.message})
     }
   };
@@ -22,20 +22,24 @@ export class PokemonController {
     const { id } = req.params;
     try {
      const pokemons = await pokemonModel.findOne({_id: id});
+     if(pokemons === null) return res.status(404).json({ "Pokemon not found _id": id });
      res.status(200).json(pokemons);
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({"error in response": error.message});
     }
   };
 
   static findOneByName = async (req: Request, res: Response) => {
     const { name } = req.params;
+    const firstLetterUpperCase = `${name[0].toUpperCase()}${name.substring(1)}`;
+
     try {
-     const pokemons = await pokemonModel.findOne({name: name});
+     const pokemons = await pokemonModel.findOne({name: firstLetterUpperCase});
+     if(pokemons === null) return res.status(404).json({ "Pokemon not found name": firstLetterUpperCase });
      res.status(200).json(pokemons);
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({"error in response": error.message});
     }
   };
@@ -46,7 +50,7 @@ export class PokemonController {
         await pokemonModel.create(newPokemon);
         res.status(201).json({ "pokemon registered successfully": newPokemon });
         
-    } catch (error:any) {
+    } catch (error) {
         res.status(404).json({ "error in request": error.message });
     }
   };
@@ -57,9 +61,9 @@ export class PokemonController {
     try {
      const pokemonUpdated = await pokemonModel.findOneAndUpdate({_id:id}, pokemon);
      if(pokemonUpdated === null) return res.status(404).json({ "Pokemon not found _id": id });
-      res.status(200).json({ "pokemon updated successfully": pokemonUpdated });
+      res.status(200).json({"pokemon updated successfully": pokemon });
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({ "error in request": error.message });
     }
   };
@@ -67,13 +71,14 @@ export class PokemonController {
   static updateByName = async (req: Request, res: Response) => {
     const { name } = req.params;
     const pokemon = req.body;
+    const firstLetterUpperCase = `${name[0].toUpperCase()}${name.substring(1)}`;
     try {
-     const pokemonUpdated = await pokemonModel.findOneAndUpdate({name: name}, pokemon);
+     const pokemonUpdated = await pokemonModel.findOneAndUpdate({name: firstLetterUpperCase}, pokemon);
 
       if(pokemonUpdated === null) return res.status(404).json({ "Pokemon not found name": name }) ;
-      res.status(200).json({ "pokemon updated successfully": pokemonUpdated });
+      res.status(200).json({ "pokemon updated successfully": pokemon });
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({ "error in request": error.message });
     }
   };
@@ -83,9 +88,10 @@ export class PokemonController {
     
     try {
       const pokemonRemoved = await pokemonModel.findByIdAndRemove(id);
+      if(pokemonRemoved === null) return res.status(404).json({ "Pokemon not found _id": id }) ;
       res.status(200).json({ "pokemon successfully removed": pokemonRemoved });
       
-    } catch (error:any) {
+    } catch (error) {
       res.status(404).json({ "error in request": error.message });
     }
   };
